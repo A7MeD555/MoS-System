9c8aa5dad1553d08c56c4ca903a912ab47144f85
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2019-01-20 17:43:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var prefix = "$";
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -8,12 +8,34 @@ client.on('ready', () => {
 
 
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$bc`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 
+client.on("message", message => {
 
-// CODES HERE
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
+ message.delete(); 
+};     
+});
 
 
+    
 
 
 
