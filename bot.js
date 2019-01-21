62f938394521e871a30d25d8dabf8e4d85177716
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2019-01-21 16:11:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$";
+var prefix = "!";
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -15,7 +15,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc | By !󠂪 󠂪󠂪󠂪A7MED .♛#1111`,"http://twitch.tv/S-F")
+client.user.setGame(`!bc  By !󠂪 󠂪󠂪󠂪A7MED .♛#1111`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
